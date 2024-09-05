f6a52efd3c64dbb40958ae2c7b845b6e0ed42bc4
kaushal <kaushalkadariya2062@gmail.com>
2024-09-05 15:50:47 +0545

    frontend url updated

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ const databaseConnection = async () => {
   }
 };
 databaseConnection();
-const frontendURL = "https://tech-r-client.vercel.app/";
+const frontendURL = "https://tech-r-client.vercel.app";
 // const frontendURL = "http://192.168.254.8:3000";
 
 app.use(
