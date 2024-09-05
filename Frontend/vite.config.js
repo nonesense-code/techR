9d14ab7f3f21127488e12c61c47d6e45ef100a90
kaushal <kaushalkadariya2062@gmail.com>
2024-09-05 16:16:26 +0545

    updated vite config js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  build: {
+    outDir: "dist", // Ensure this directory is where your files are properly built
+  },
+  server: {
+    strictPort: true,
+  },
   server: {
     proxy: {
       "product/api": "https://react-app-api-tau.vercel.app",
