c035bba7ba1abe4e23a937b8424708af8acc94d4
kaushal <kaushalkadariya2062@gmail.com>
2024-10-17 10:59:04 +0545

    updated

--- Frontend/src/components/HomePage/Home.jsx
+++ Frontend/src/components/HomePage/Home.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 import Landing from "./Landing";
-
 function Home() {
-  const fetchData = async () => {
-    const response = await axios
-      .get("https://tech-r.vercel.app/sitemap.xml")
-      .then((response) => {
-        res.set("Content-Type", "application/xml");
-        res.send(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching sitemap:", error);
-        res.status(500).send("Error fetching sitemap");
-      });
-  };
-  fetchData();
-
   return (
     <div>
       <Landing />
--- Frontend/src/components/HomePage/Landing.jsx
+++ Frontend/src/components/HomePage/Landing.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { motion } from "framer-motion";
 import CircularLoader from "../../CircularLoader";
@@ -12,7 +11,7 @@ import samsumgtab from "../../images/galaxytabs9ultra.jpg";
 import acer from "../../images/acernitrov15.png";
 import Huwai from "../../images/huaweimatext.jpg";
 import asus from "../../images/Asus-ROG-Zephyrus-G14-image.jpg";
-
+import axios from "axios";
 import { useQuery } from "react-query";
 const filterProducts = async (url) => {
   try {
@@ -25,7 +24,6 @@ const filterProducts = async (url) => {
 function Landing() {
   const latestURL = import.meta.env.VITE_LATEST_URL;
   const mostpopularURL = import.meta.env.VITE_MOSTPOPULAR_URL;
-  const popularityURL = import.meta.env.VITE_POPULARITY_URL;
   const mostsoldURL = import.meta.env.VITE_MOSTSOLD_URL;
   const budgetURL = import.meta.env.VITE_BUDGET_URL;
   const midrangeURL = import.meta.env.VITE_MIDRANGE_URL;
@@ -126,22 +124,9 @@ function Landing() {
     }
   );
 
-  const {
-    isLoading: loadingPopularity,
-    data: popularity = [],
-    popularityisError,
-    popularityError,
-  } = useQuery(
-    ["popularity", popularityURL],
-    () => filterProducts(popularityURL),
-    {
-      staleTime: 1000 * 60 * 5,
-    }
-  );
   let isLoading =
     loadingLatest ||
     loadingMostpopular ||
-    loadingPopularity ||
     loadingMostsold ||
     loadingBudget ||
     loadingMidrange ||
@@ -163,66 +148,68 @@ function Landing() {
         </Helmet>
         <AIlanding />
         <div className="h-full min-w-full sm:min-w-0 w-auto md:max-w-[1200px]">
-          <h1 className="text-center text-2xl font-bold tracking-wide">
-            Distinguished Picks
-          </h1>
-          <div className="min-w-full w-full flex flex-wrap border-2 border-stone-600 mt-2">
-            <div className="flex-1 min-w-0 w-full md:w-1/3 border-r-2 border-black p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
-              <Link
-                to="/phone"
-                className="h-full items-center flex justify-center w-auto p-2 text-black"
-              >
-                <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
-                  <CiMobile3 className="text-3xl scale-125 md:scale-150" />
-                  <h1 className="text-xs md:text-sm">Phones</h1>
-                </div>
-              </Link>
-            </div>
+          <div className="min-w-[1200px] max-w-[1200px]">
+            <h1 className="text-center text-2xl font-bold tracking-wide">
+              Distinguished Picks
+            </h1>
+            <div className="min-w-full w-full flex flex-wrap border-2 border-stone-600 mt-2">
+              <div className="flex-1 min-w-0 w-full md:w-1/3 border-r-2 border-black p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
+                <Link
+                  to="/phone"
+                  className="h-full items-center flex justify-center w-auto p-2 text-black"
+                >
+                  <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
+                    <CiMobile3 className="text-3xl scale-125 md:scale-150" />
+                    <h1 className="text-xs md:text-sm">Phones</h1>
+                  </div>
+                </Link>
+              </div>
 
-            <div className="flex-1 min-w-0 w-full md:w-1/3 border-r-2 border-black p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
-              <Link
-                to="/laptop"
-                className="h-full items-center flex justify-center w-auto p-2 text-black"
-              >
-                <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
-                  <LiaLaptopSolid className="text-3xl scale-125 md:scale-150" />
-                  <h1 className="text-xs md:text-sm">Laptops</h1>
-                </div>
-              </Link>
-            </div>
+              <div className="flex-1 min-w-0 w-full md:w-1/3 border-r-2 border-black p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
+                <Link
+                  to="/laptop"
+                  className="h-full items-center flex justify-center w-auto p-2 text-black"
+                >
+                  <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
+                    <LiaLaptopSolid className="text-3xl scale-125 md:scale-150" />
+                    <h1 className="text-xs md:text-sm">Laptops</h1>
+                  </div>
+                </Link>
+              </div>
 
-            <div className="flex-1 min-w-0 w-full md:w-1/3 p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
-              <Link
-                to="/tablet"
-                className="h-full items-center flex justify-center w-auto p-2 text-black"
-              >
-                <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
-                  <FaTabletAlt className="text-3xl scale-125 md:scale-150" />
-                  <h1 className="text-xs md:text-sm">Tablets</h1>
-                </div>
-              </Link>
+              <div className="flex-1 min-w-0 w-full md:w-1/3 p-2 text-center hover:bg-sky-600/30 active:bg-sky-600/40 overflow-hidden">
+                <Link
+                  to="/tablet"
+                  className="h-full items-center flex justify-center w-auto p-2 text-black"
+                >
+                  <div className="flex flex-col items-center justify-center gap-4 text-[#232F3E]">
+                    <FaTabletAlt className="text-3xl scale-125 md:scale-150" />
+                    <h1 className="text-xs md:text-sm">Tablets</h1>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center justify-center gap-2 mt-10">
-            <div className="text-center text-2xl font-bold tracking-wide">
-              Top Deals
-            </div>
-            <div className="grid grid-cols-2 md:grid-cols-2 relative h-full w-full gap-1">
-              {popular_items.map((item, index) => (
-                <div key={index} className="relative w-full overflow-hidden">
-                  <Link to={item.link}>
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="border-b-2 border-white/70 object-cover h-[160px] md:h-[240px] lg:h-[300px] w-full bg-black border-r-2 border-white hover:scale-105 duration-200 ease-out"
-                      loading="lazy"
-                    />
-                    <div className="absolute text-center bg-black border-white border-t-2 border-b-2 text-[#00FFA3] w-full bottom-0 h-auto p-2 mx-auto left-0 right-0">
-                      {item.name}
-                    </div>
-                  </Link>
-                </div>
-              ))}
+            <div className="flex flex-col items-center justify-center gap-2 mt-10">
+              <div className="text-center text-2xl font-bold tracking-wide">
+                Top Deals
+              </div>
+              <div className="grid grid-cols-2 md:grid-cols-2 relative h-full w-full gap-1">
+                {popular_items.map((item, index) => (
+                  <div key={index} className="relative w-full overflow-hidden">
+                    <Link to={item.link}>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="border-b-2 border-white/70 object-cover h-[160px] md:h-[240px] lg:h-[300px] w-full bg-black border-r-2 border-white hover:scale-105 duration-200 ease-out"
+                        loading="lazy"
+                      />
+                      <div className="absolute text-center bg-black border-white border-t-2 border-b-2 text-[#00FFA3] w-full bottom-0 h-auto p-2 mx-auto left-0 right-0">
+                        {item.name}
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
           {!isLoading ? (
